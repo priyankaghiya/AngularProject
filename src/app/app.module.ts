19a3fa31eba249db255c5fe7be1b9c960bff526d
Priyanka Ghiya <piyughiya12@gmail.com>
2021-04-26 19:19:24 -0700

    Two Way Binding Using ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-//import { FormsModule} from '@angular/forms'
+import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -14,7 +14,7 @@ import { ServersComponent } from './servers/servers.component';
   ],
   imports: [
     BrowserModule,
-    //FormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
