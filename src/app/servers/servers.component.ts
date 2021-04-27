0e5317f77d426844bb961af5de321bd048bee1cd
Priyanka Ghiya <piyughiya12@gmail.com>
2021-04-26 23:40:00 -0700

    Assignment 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus =" No server created";
+  username = '';
   serverName = '';
   constructor() { 
     setTimeout(() => {
